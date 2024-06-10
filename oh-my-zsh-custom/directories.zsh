bf2c184583d65f8582f07ecfcb1343dc00bdab40
Christopher Bradshaw <chris.kofox@gmail.com>
2024-06-10 16:15:30 -0600

    Auto-save updates to settings

--- oh-my-zsh-custom/directories.zsh
+++ oh-my-zsh-custom/directories.zsh
@@ -1,3 +1,3 @@
-SETTINGS_DIR="${HOME}/Git/kitsune-settings"
-ICLOUD_DIR="${HOME}/Library/Mobile Documents/com~apple~CloudDocs"
-ICLOUD_BACKUP_DIR="${ICLOUD_DIR}/Resources/1 - Backup"
+export SETTINGS_DIR="${HOME}/Git/kitsune-settings"
+export ICLOUD_DIR="${HOME}/Library/Mobile Documents/com~apple~CloudDocs"
+export ICLOUD_BACKUP_DIR="${ICLOUD_DIR}/Resources/1 - Backup"
