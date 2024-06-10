83d6339ab92ee5a1e4e1a2991e9311c081ec6938
Christopher Bradshaw <chris.kofox@gmail.com>
2024-06-10 14:42:08 -0600

    Auto-save updates to settings

--- oh-my-zsh-custom/aliases.zsh
+++ oh-my-zsh-custom/aliases.zsh
@@ -183,7 +183,7 @@ function restore-workrc() {
 
 function go-latest() {
   download_page=$(curl -sL "https://go.dev/dl/?mode=json")
-  latest_version=$(echo "$download_page" | jq -r '.version')
+  latest_version=$(echo "$download_page" | jq -r '.[0].version')
 
   if [[ -z "$latest_version" ]]; then
     echo "Error: Failed to retrieve latest Go version."
