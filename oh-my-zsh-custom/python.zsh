7426c9e63d133de2c400c48537ce9eef41f3e22d
Christopher Bradshaw <chris.kofox@gmail.com>
2024-07-10 11:18:20 -0600

    Auto-save updates to settings

--- oh-my-zsh-custom/python.zsh
+++ oh-my-zsh-custom/python.zsh
@@ -1,4 +1,8 @@
 alias activate="source venv/bin/activate"
+alias a="activate"
+alias d="deactivate"
+alias j="jupyter"
+alias py-run="python3 src/main.py"
 
 function create-py() {
   projectName=$1
@@ -9,5 +13,11 @@ function create-py() {
   source venv/bin/activate
   python3 -m pip install --upgrade pip
   python3 -m pip --version
+
+  git init
+  echo "# $projectName" >> README.md
+  mkdir src
+  touch src/main.py
+
   code .
 }
