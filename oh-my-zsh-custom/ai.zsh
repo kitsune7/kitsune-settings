145db43aa7876ca13831ac557696b3f350def793
Christopher Bradshaw <chris.kofox@gmail.com>
2025-02-04 13:23:09 -0700

    Auto-save updates to settings

--- oh-my-zsh-custom/ai.zsh
+++ oh-my-zsh-custom/ai.zsh
@@ -4,7 +4,8 @@ alias os="ori-server"
 function ori () {
   ori-server > /dev/null & server_pid=$!
   sleep 1
-  curl -X POST http://localhost:1230/chat/completions \
+
+  response=$(curl -s -X POST http://localhost:1230/chat/completions \
     -H "Content-Type: application/json" \
     -d '{
       "model": "qwen2.5-7b-instruct-1m",
@@ -14,9 +15,10 @@ function ori () {
           "content": "'$1'"
         }
       ]
-    }' | jq -r '.completions[0].message.content'
+    }')
   curl_exit_code=$?
   kill $server_pid
+  
   if [ $curl_exit_code -ne 0 ]; then
     echo "Ori failed to start or respond appropriately."
     exit 1
