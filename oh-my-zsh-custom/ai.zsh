3b60dd06acbdae63998b6c7f67f02593ca6f9b40
Christopher Bradshaw <chris.kofox@gmail.com>
2025-02-04 13:02:13 -0700

    Auto-save updates to settings

--- oh-my-zsh-custom/ai.zsh
+++ oh-my-zsh-custom/ai.zsh
@@ -1,1 +1,13 @@
-alias ori="${SETTINGS_DIR}/custom-scripts/ori.py"
+alias ori-server="${SETTINGS_DIR}/custom-scripts/ori.py"
+alias os="ori-server"
+
+function ori () {
+  ori-server & server_pid=$!
+  sleep 1
+  curl -f http://localhost:1230
+  curl_exit_code=$?
+  kill $server_pid
+  if [ $curl_exit_code -ne 0 ]; then
+    echo "Server failed to start and accept connections"
+  fi
+}
