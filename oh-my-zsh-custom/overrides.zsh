710c04e77cddad403ebabe18816b9f9ec97082c1
Christopher Bradshaw <chris.kofox@gmail.com>
2025-04-23 15:11:03 -0600

    Auto-save updates to settings

--- oh-my-zsh-custom/overrides.zsh
+++ oh-my-zsh-custom/overrides.zsh
@@ -5,3 +5,12 @@ alias python="python3"
 alias pip="python3 -m pip"
 alias vi="vim"
 alias sha256sum='shasum --algorithm 256'
+
+# Use Cursor instead of VSCode by default
+function code () {
+  if command -v cursor >/dev/null 2>&1; then
+    cursor "$@"
+  else
+    /usr/local/bin/code "$@"
+  fi
+}
