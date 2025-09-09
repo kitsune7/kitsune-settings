1b1cc63946d3c95173b4370a1ea0b4aa251dc31d
Christopher Bradshaw <chris.kofox@gmail.com>
2025-09-09 14:04:52 -0600

    Auto-save updates to settings

--- oh-my-zsh-custom/python.zsh
+++ oh-my-zsh-custom/python.zsh
@@ -2,7 +2,7 @@ alias activate="source venv/bin/activate"
 alias a="activate"
 alias d="deactivate"
 alias j="jupyter"
-alias py-run="python3 src/main.py"
+alias py-run="python src/main.py"
 
 function create-py () {
   projectName=$1
@@ -22,9 +22,7 @@ function create-py () {
 function venv () {
   if [ ! -d "venv" ]; then
     echo "No virtual environment found. Creating one..."
-    python3 -m venv venv
-    python3 -m pip install --upgrade pip
-    python3 -m pip --version
+    python -m venv venv
   fi
   source venv/bin/activate
 }
