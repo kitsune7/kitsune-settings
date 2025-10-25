f520b431afb4dbd744a570e87f64e33c0b378fca
Christopher Bradshaw <chris.kofox@gmail.com>
2025-10-25 13:52:40 -0600

    Auto-save updates to settings

--- oh-my-zsh-custom/python.zsh
+++ oh-my-zsh-custom/python.zsh
@@ -6,17 +6,45 @@ alias py-run="python src/main.py"
 
 function create-py () {
   projectName=$1
+  description=${2:-"A new Python project"}
+  moduleName=$(echo "$projectName" | perl -pe 's/-/_/g')
 
-  mkdir $projectName
+  mkdir -p "${projectName}/src/${moduleName}"
+  mkdir -p "${projectName}/tests"
   cd $projectName
-  venv
-
   git init
-  echo "# $projectName" >> README.md
-  mkdir src
-  touch src/main.py
+  uv init
+  gitignore
+  mit-license
 
-  ide .
+  echo '"""
+Initialization file for the ${moduleName} module.
+"""
+
+__version__ = "0.1.0"
+' > "${projectName}/src/${moduleName}/__init__.py"
+  replace-in-file '    main()' "    sys.exit(main())" hello.py
+  mv hello.py "src/${moduleName}/cli.py"
+
+  replace-in-file 'Add your description here' "$description" pyproject.toml
+  echo "
+[project.scripts]
+${projectName} = \"${moduleName}.cli:main\"
+
+[build-system]
+requires = [\"hatchling\"]
+build-backend = \"hatchling.build\"
+
+[tool.hatch.build.targets.wheel]
+packages = [\"src/${moduleName}\"]
+" >> pyproject.toml
+  uv add --dev ruff pytest
+  echo '
+  [tool.ruff]
+  line-length = 100
+  lint.select = ["E", "F", "I", "N", "W"]
+  ' >> pyproject.toml
+  replace-in-file '    ' '  ' pyproject.toml
 }
 
 function venv () {
